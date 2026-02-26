7aaa5eed269a8934b5c5b85a53ed2f0a524c85e6
이가연 <yeonie0309@naver.com>
2026-02-26 21:12:57 +0900

    naver login

--- src/main/java/com/springboot/security/config/SecurityConfig.java
+++ src/main/java/com/springboot/security/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .oauth2Login(oauth -> oauth
                         .defaultSuccessUrl("/")
                         .userInfoEndpoint(userInfo -> userInfo
+
                                 .userService(customOAuth2UserService)
                         )
                 );
--- src/main/java/com/springboot/security/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/security/dto/OAuthAttributes.java
@@ -25,7 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-
+        if ("naver".equals(registrationId)) {
+            return ofNaver(attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +38,21 @@ public class OAuthAttributes {
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static OAuthAttributes ofNaver(Map<String, Object> attributes) {
+
+        Map<String, Object> response =
+                (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey("id")
                 .build();
     }
 
